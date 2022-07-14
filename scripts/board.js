40f362cdb3c2106940d36cc68df3934ceee61a3b
Rickson Lima <rickson.lima.dev@gmail.com>
2022-07-14 09:25:53 -0400

    chore: move board mode logic to separated file

---
+++ scripts/board.js
@@ -0,0 +1,32 @@
+let boardMode = "morning";
+let width = 100;
+
+const decrementBoardWidth = () => width--;
+const incrementBoardWidth = () => width++;
+
+function toggleBoardMode() {
+  boardMode === "morning" ? nightBoard() : morningBoard();
+}
+
+function resizeBoardWidth() {
+  if (width < 100 && boardMode === "morning") {
+    incrementBoardWidth();
+    gameBoard.style.width = `${width}%`;
+  }
+  if (boardMode === "night") {
+    decrementBoardWidth();
+    gameBoard.style.width = `${width}%`;
+  }
+}
+
+function nightBoard() {
+  boardMode = "night";
+  gameBoard.style.background = "var(--night-bg)";
+  score.style.color = "var(--night-score)";
+}
+
+function morningBoard() {
+  boardMode = "morning";
+  score.style.color = "var(--morning-score)";
+  gameBoard.style.background = "var(--morning-bg)";
+}
--- scripts/score.js
+++ scripts/score.js
@@ -1,36 +1,4 @@
-let boardMode = "morning";
 let currentScore = 0;
-let width = 100;
-
-const decrementBoardWidth = () => width--;
-const incrementBoardWidth = () => width++;
-
-function toggleBoardMode() {
-  boardMode === "morning" ? nightBoard() : morningBoard();
-}
-
-function resizeBoardWidth() {
-  if (width < 100 && boardMode === "morning") {
-    incrementBoardWidth();
-    gameBoard.style.width = `${width}%`;
-  }
-  if (boardMode === "night") {
-    decrementBoardWidth();
-    gameBoard.style.width = `${width}%`;
-  }
-}
-
-function nightBoard() {
-  boardMode = "night";
-  gameBoard.style.background = "var(--night-bg)";
-  score.style.color = "var(--night-score)";
-}
-
-function morningBoard() {
-  boardMode = "morning";
-  score.style.color = "var(--morning-score)";
-  gameBoard.style.background = "var(--morning-bg)";
-}
 
 const loopScore = setInterval(() => {
   currentScore = currentScore + 1;
